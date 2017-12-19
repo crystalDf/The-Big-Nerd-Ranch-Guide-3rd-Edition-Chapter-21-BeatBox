f32f8013914c08bb795574fbdb29818eb4da7be2
crystalDf <chendong333@gmail.com>
2017-12-19 14:00:15 +0800

    Chapter 21 BeatBox

---
+++ app/src/main/java/com/star/beatbox/BeatBox.java
@@ -0,0 +1,45 @@
+package com.star.beatbox;
+
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeatBox {
+
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+
+    private AssetManager mAssetManager;
+    private List<Sound> mSounds = new ArrayList<>();
+
+    public BeatBox(Context context) {
+        mAssetManager = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds() {
+        String[] soundNames;
+        try {
+            soundNames = mAssetManager.list(SOUNDS_FOLDER);
+
+            for (String filename : soundNames) {
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                mSounds.add(sound);
+            }
+
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException e) {
+            Log.e(TAG, "Could not list assets", e);
+        }
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
+    }
+}
